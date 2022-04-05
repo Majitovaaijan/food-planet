af5e96ed94d8f1c76a3baf613148ce180cc34d6f
Aliya <kozhobekova0424@gmail.com>
2022-04-05 10:14:11 +0600

    feat: added MenuForPizza.jsx

---
+++ src/components/MenuForPizza/MenuForPizza.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PizzaMenu from './PizzaMenu';
+import Pizzas from "./Pizzas";
+
+const MenuForPizza = () => {
+    return (
+        <div>
+            <PizzaMenu/>
+            <Pizzas/>
+        </div>
+    );
+};
+
+export default MenuForPizza;
