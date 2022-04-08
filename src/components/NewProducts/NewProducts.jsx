d767560b3248eeeb3d39401d9588e8cfd38e1c24
Aijan <aijanka.9239@gmail.com>
2022-04-08 13:37:43 +0600

    feat: added functions in NewProducts

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./NewProduct.module.css"
 import img from '../../media/second.svg'
 
 const NewProducts = (props) => {
+    const [number, setNumber] = useState(1)
+    const increment = () =>{
+        setNumber(number +1)
+    }
+    const decrement = () =>{
+        setNumber(number -1)
+    }
     return (
             <div className={styles.burgers}>
                 <div className={styles.burger}>
@@ -11,9 +18,9 @@ const NewProducts = (props) => {
                     <p className={styles.composition}>{props.item.composition}</p>
                     <p className={styles.price}>{props.item.price}</p>
                     <div className={styles.btn}>
-                        <button className={styles.minus}>-</button>
-                        <p className={styles.number}>1</p>
-                        <button className={styles.plus}>+</button>
+                        <button onClick={decrement} className={styles.minus}>-</button>
+                        <p className={styles.number}>{number}</p>
+                        <button onClick={increment} className={styles.plus}>+</button>
                     </div>
                     <button className={styles.basket}>В корзину</button>
                 </div>
